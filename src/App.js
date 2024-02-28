9e9f54db3a40ea48a2e80636b0ab8266d948435f
Libin Rahman <rahmanlibi47@gmail.com>
2024-02-28 13:53:59 +0530

    Updating error fixe

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import Navbar from "./components/navbar";
+import Navbars from "./components/navbar";
 
 function App() {
   return (
@@ -8,7 +8,7 @@ function App() {
         <div>
           <div>
             <div>
-              <Navbar />
+              <Navbars />
               <h2>Test Deploy</h2>
               <button>Button</button>
             </div>
