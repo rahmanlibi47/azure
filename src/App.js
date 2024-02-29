7337c5f65a62cb9c55234d42e5c64134ef3e03b3
Libin Rahman <rahmanlibi47@gmail.com>
2024-02-29 20:26:33 +0530

    removing zipping

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import "./App.css";
 import Navbars from "./components/navbar";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import AccessibilityNewIcon from '@mui/icons-material/AccessibilityNew';
+
 function App() {
   return (
     <div className="App">
@@ -13,6 +15,7 @@ function App() {
               <h2 cl>New Services</h2>
               <button className="btn btn-danger">Button</button>
               <button className="btn btn-success">New</button>
+              <AccessibilityNewIcon />
             </div>
           </div>
         </div>
