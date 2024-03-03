8738cc67f568e26445b967e2bc6713b3406c45cb
Libin Rahman <rahmanlibi47@gmail.com>
2024-03-03 20:45:54 +0530

    Updating bgm

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import Swal from 'sweetalert2';
 
 const Home = () => {
+  const [audio] = useState(new Audio(process.env.PUBLIC_URL + '/bgm.mp3'));
+
+  useEffect(() => {
+    Swal.fire({
+      title: 'Welcome to the space journey!',
+      confirmButtonText: 'OK'
+    }).then(() => {
+      audio.play();
+    });
+
+    return () => {
+      audio.pause();
+    };
+  }, [audio]);
+
   return (
     <div>
       <h2>Homepage</h2>
-      <br/>
+      <br />
       <p>Welcome to home</p>
     </div>
   )
 }
 
-export default Home
+export default Home;
