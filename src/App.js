04d3942b2a12601ed881713fe6f02b248f6416e4
Libin Rahman <rahmanlibi47@gmail.com>
2024-03-04 22:50:18 +0530

    Updating rotation || navbar toggle

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useEffect, useState } from 'react';
 import "./App.css";
 import Navbars from "./components/navbar";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
@@ -5,10 +6,23 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 
 function App() {
+  const [rotate, setRotate] = useState(true);
+
+  useEffect(() => {
+    const body = document.body;
+    if (rotate) {
+      body.classList.remove('static');
+      body.classList.add('rotate');
+    } else {
+      body.classList.remove('rotate');
+      body.classList.add('static');
+    }
+  }, [rotate]);
+  
   return (
     <Router>
     <div className="App">
-      <Navbars />
+    <Navbars rotate={rotate} setRotate={setRotate} />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="about" element={<About />} />
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,17 +1,31 @@
+import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import RocketLaunchIcon from '@mui/icons-material/RocketLaunch';
+import Switch from '@mui/material/Switch';
+import LoopIcon from '@mui/icons-material/Loop';
 
-function Navbars() {
+function Navbars({ rotate, setRotate }) {
+  const handleToggle = (event, newValue) => {
+    setRotate(newValue);
+  };
+
   return (
-    <Navbar className="bg-dark">
+    <Navbar className="bg-dark navbar-opacity">
       <Container  >
-        <Navbar.Brand style={{color:'white'}} href="/">Coderz</Navbar.Brand>
+        <Navbar.Brand style={{ color: 'white' }} href="/"><RocketLaunchIcon /></Navbar.Brand>
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav  className="me-auto">
-            <Nav.Link className='hover-nav' style={{color:'white'}} href="/">Home</Nav.Link>
-            <Nav.Link className='hover-nav' style={{color:'white'}} href="/about">About</Nav.Link>
-          </Nav>
+        <Nav className="me-auto" style={{ display: 'flex', justifyContent: 'space-between', width: '100%' }}>
+  <div style={{ display: 'flex' }}>
+    <Nav.Link className='hover-nav' style={{ color: 'white' }} href="/">Home</Nav.Link>
+    <Nav.Link className='hover-nav' style={{ color: 'white' }} href="/about">About</Nav.Link>
+  </div>
+  <div style={{ display: 'flex', alignItems: 'center' }}>
+  <span style={{ marginRight: '10px', color: 'white' }}><LoopIcon/></span>
+  <Switch checked={rotate} onChange={handleToggle} />
+</div>
+</Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
